e91a7410905c6266110dc3376057d082f0645ae1
Ashley Hindle <hey@ashleyhindle.com>
2019-01-20 01:06:21 +0000

    Improve Config unit tests

--- tests/PrivateDump/ConfigTest.php
+++ tests/PrivateDump/ConfigTest.php
@@ -8,26 +8,29 @@ class ConfigTest extends PHPUnit_Framework_TestCase
     private $config;
     private $filename = '';
 
-    protected function setUp()
+    /** @test */
+    public function weCanOverrideMySQLConfig()
     {
-        $this->filename = tempnam(sys_get_temp_dir(), 'private-dump');
-        file_put_contents($this->filename, '{"mysql": {"username": "root", "password": "bigben", "hostname": "localhost"}, "databases": {"test": {}}}');
-
-        $this->config = new Config($this->filename, ['mysql' => [
+        $this->getConfig('{"mysql": {"username": "root", "password": "bigben", "hostname": "localhost"}, "databases": {"test": {}}}', ['mysql' => [
             'password' => 'expelliarmus'
         ]]);
-        $this->config->parseConfig();
+
+        $this->assertEquals('expelliarmus', $this->config->get('mysql.password'));
     }
 
-    protected function tearDown()
+    /** @test */
+    public function weGetAnErrorWithABadFile()
     {
-        parent::tearDown();
-        unlink($this->filename);
+        $this->assertFalse((new Config('/non/existent/file/ever.json'))->isValid());
+        $this->assertFalse($this->getConfig('BAD JSON')->isValid());
+
+        // No databases
+        $this->assertFalse($this->getConfig('{"mysql": {"username": "root", "password": "bigben", "hostname": "localhost"}')->isValid());
     }
 
     public function testGet()
     {
-        $this->assertEquals('expelliarmus', $this->config->get('mysql.password'));
+        $this->assertEquals('bigben', $this->config->get('mysql.password'));
         $this->assertEquals('localhost', $this->config->get('mysql.hostname'));
         $this->assertEquals('root', $this->config->get('mysql.username'));
     }
@@ -41,5 +44,28 @@ class ConfigTest extends PHPUnit_Framework_TestCase
     public function testIsValid()
     {
         $this->assertTrue($this->config->isValid());
+    }
+
+
+    protected function setUp()
+    {
+        $this->getConfig('{"mysql": {"username": "root", "password": "bigben", "hostname": "localhost"}, "databases": {"test": {}}}');
+    }
+
+    protected function getConfig($json, $overrides=[])
+    {
+        $this->filename = tempnam(sys_get_temp_dir(), 'private-dump');
+        file_put_contents($this->filename, $json);
+
+        $this->config = new Config($this->filename, $overrides);
+        $this->config->parseConfig();
+
+        return $this->config;
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        unlink($this->filename);
     }
 }
